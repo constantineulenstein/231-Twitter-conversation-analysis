510e14adf0855ad8c215cd7a725e9ea036a13136
Axel Peytavin <peytavin@stanford.edu>
2022-10-25 02:02:56 -0700

    Updates from monday evening

--- get_legislators.py
+++ get_legislators.py
@@ -4,29 +4,37 @@ import pandas as pd
 from yaml import safe_load
 import pickle
 
-with open('legislators-current.yaml', 'r') as f:
+with open("legislators-current.yaml", "r") as f:
     legs_current = pd.json_normalize(safe_load(f))
 
 party = pd.Series([row[-1]["party"] for row in legs_current["terms"]])
-legs_current = legs_current[["name.official_full", "id.bioguide", "id.thomas", "id.govtrack"]]
+legs_current = legs_current[
+    ["name.official_full", "id.bioguide", "id.thomas", "id.govtrack"]
+]
 legs_current["party"] = party
-legs_current = legs_current[legs_current["name.official_full"]=="Elizabeth Warren"]
+# legs_current = legs_current[legs_current["name.official_full"]=="Elizabeth Warren"]
 
 
-with open('legislators-social-media.yaml', 'r') as f:
+with open("legislators-social-media.yaml", "r") as f:
     legs_social = pd.json_normalize(safe_load(f))
 
-legs_social = legs_social[["id.bioguide", "id.thomas", "id.govtrack", "social.twitter_id", "social.twitter"]]
+legs_social = legs_social[
+    ["id.bioguide", "id.thomas", "id.govtrack", "social.twitter_id", "social.twitter"]
+]
 
-legs = pd.merge(legs_current, legs_social, how='inner', left_on=["id.bioguide", "id.thomas", "id.govtrack"],
-                right_on=["id.bioguide", "id.thomas", "id.govtrack"])
+legs = pd.merge(
+    legs_current,
+    legs_social,
+    how="inner",
+    left_on=["id.bioguide", "id.thomas", "id.govtrack"],
+    right_on=["id.bioguide", "id.thomas", "id.govtrack"],
+)
 legs = legs.drop(["id.bioguide", "id.thomas", "id.govtrack"], axis=1)
 legs = legs[(legs.party == "Democrat") | (legs.party == "Republican")]
 
 legs.to_pickle("legislators")
 
 
-#Population 1: Democrats
-#Population 2: Republicans
-#Hypothesis: Republican trees go deeper, have wider spread, and larger cascade
-
+# Population 1: Democrats
+# Population 2: Republicans
+# Hypothesis: Republican trees go deeper, have wider spread, and larger cascade
--- tweet_stream.py
+++ tweet_stream.py
@@ -3,8 +3,10 @@
 import sys
 import time
 import pandas as pd
+import json
 import datetime
 from treelib import Node, Tree
+from pathlib import Path
 
 from tweepy import Stream, Client, StreamingClient, StreamRule, Paginator
 
@@ -18,43 +20,70 @@ def create_twitter_client():
             creds[key] = value
 
     return Client(
-        creds["bearer_token"], creds["api_key"], creds["api_secret"], creds["token"], creds["token_secret"]
+        creds["bearer_token"],
+        creds["api_key"],
+        creds["api_secret"],
+        creds["token"],
+        creds["token_secret"],
     )
 
 
-def create_tree(conv_id, client):
-    tree = Tree()
-    conversation = client.search_recent_tweets(f"conversation_id:{conv_id}",
-                                               expansions=['referenced_tweets.id.author_id',
-                                                           "in_reply_to_user_id"],
-                                               tweet_fields=["in_reply_to_user_id"])
-    tree.create_node(conv_id, conv_id)  # Create Root Node
+def get_conversation_data(conv_id, client):
+    request_args = {
+        "query": f"conversation_id:{conv_id}",
+        "expansions": ["referenced_tweets.id.author_id", "in_reply_to_user_id"],
+        "tweet_fields": ["in_reply_to_user_id"],
+    }
+    conversation = client.search_recent_tweets(**request_args)
 
-    conversation_tree_data = []  # list to store all conversation interactions -> later reverse it because twitter
+    conversation_data = []
+    # list to store all conversation interactions -> later reverse it because twitter
     # api is in reversed chronological order
 
     for conv in conversation.data:
-        conversation_tree_data.append((conv["id"], conv["referenced_tweets"][0]["id"]))
+        conversation_data.append((conv["id"], conv["referenced_tweets"][0]["id"]))
 
     while "next_token" in conversation.meta:
-        conversation = client.search_recent_tweets(f"conversation_id:{conv_id}",
-                                                   expansions=['referenced_tweets.id.author_id',
-                                                               "in_reply_to_user_id"],
-                                                   tweet_fields=["in_reply_to_user_id"],
-                                                   next_token=conversation.meta["next_token"])
+        conversation = client.search_recent_tweets(
+            **request_args,
+            next_token=conversation.meta["next_token"],
+        )
 
         for conv in conversation.data:
-            conversation_tree_data.append((conv["id"], conv["referenced_tweets"][0]["id"]))
+            conversation_data.append((conv["id"], conv["referenced_tweets"][0]["id"]))
 
-    error_count = 0
-    for tree_datapoint in conversation_tree_data[::-1]:  # reverse list
-        try:
-            tree.create_node(tree_datapoint[0], tree_datapoint[0], parent=tree_datapoint[1])
-        except:
-            error_count += 1
-    tree.show()
-    print(f"Created Tree with {len(conversation_tree_data) - error_count} replies. {error_count} replies were throwing "
-          f"an error due to deletions.")
+    return conversation_data[::-1]
+
+
+def retrieve_and_populate_tweets_data_for_user(
+    client, user_id: int, official_name: str, next_token: str = None
+):
+    today = datetime.datetime.today()
+    week_ago = today - datetime.timedelta(days=7)
+
+    request_args = {
+        "id": user_id,
+        "expansions": ["referenced_tweets.id.author_id"],
+        "start_time": week_ago,
+        "tweet_fields": ["conversation_id,author_id"],
+        "max_results": 100,
+    }
+
+    populated_tweets = []
+
+    tweets = client.get_users_tweets(**request_args, pagination_token=next_token)
+
+    for tweet in tweets.data:
+        if tweet.referenced_tweets is None:
+            tweet_data = {
+                "full_name": official_name,
+                "author_id": tweet["author_id"],
+                "conversation_id": tweet["conversation_id"],
+                "tweet_text": tweet["text"],
+            }
+            populated_tweets.append(tweet_data)
+
+    return populated_tweets, tweets.meta
 
 
 def get_legislator_tweets(client):
@@ -62,41 +91,96 @@ def get_legislator_tweets(client):
     democrat_tweets = []
     republican_tweets = []
 
-    today = datetime.datetime.today()
-    week_ago = today - datetime.timedelta(days=7)
-
     for idx in legs.index:
-        tweets = client.get_users_tweets(int(legs["social.twitter_id"][idx]),
-                                         expansions=['referenced_tweets.id.author_id'], start_time=week_ago,
-                                         tweet_fields=["conversation_id,author_id"])
-        for tweet in tweets.data:
-            if tweet.referenced_tweets is None:
-                democrat_tweets.append(
-                    [legs["name.official_full"][idx], tweet["author_id"], tweet["conversation_id"], tweet["text"]]) if \
-                    legs["party"][idx] == "Democrat" else republican_tweets.append([tweet["id"], tweet["text"]])
+        try:
+            user_id = int(legs["social.twitter_id"][idx])
+        except ValueError:
+            print("Twitter id not present for", official_name)
+        official_name = legs["name.official_full"][idx]
+        party = legs["party"][idx]
 
-        while "next_token" in tweets.meta:
-            tweets = client.get_users_tweets(int(legs["social.twitter_id"][idx]),
-                                             expansions=['referenced_tweets.id.author_id'], start_time=week_ago,
-                                             tweet_fields=["conversation_id"],
-                                             pagination_token=tweets.meta["next_token"])
+        print(
+            "Total tweets pulled so far:",
+            len(democrat_tweets),
+            "democrats, and",
+            len(republican_tweets),
+            "republican",
+        )
+        print("Pulling tweets for legislator", official_name, "of the", party, "party")
 
-            for tweet in tweets.data:
-                if tweet.referenced_tweets is None:
-                    democrat_tweets.append(
-                        [legs["name.official_full"][idx], tweet["author_id"], tweet["conversation_id"],
-                         tweet["text"]]) if \
-                        legs["party"][idx] == "Democrat" else republican_tweets.append([tweet["id"], tweet["text"]])
+        try:
+            tweets, tweets_meta = retrieve_and_populate_tweets_data_for_user(
+                client,
+                user_id,
+                official_name,
+            )
+        except TypeError:
+            # For example, Lindsey Graham's account in the data we pulled is not the
+            # one he uses, it's a very old one.
+            print("No tweets found for", official_name, ", is the account correct ?")
 
-        if idx == 1:  # This is here to only fetch last twenty tweets. If removed, it will fetch the last 7 days
-            break
+        while "next_token" in tweets_meta:
+            new_tweets, tweets_meta = retrieve_and_populate_tweets_data_for_user(
+                client,
+                user_id,
+                official_name,
+                tweets_meta["next_token"],
+            )
 
-        return democrat_tweets, republican_tweets
+            tweets += new_tweets
+
+        if party == "Democrat":
+            democrat_tweets += tweets
+        else:
+            republican_tweets += tweets
+
+    return democrat_tweets, republican_tweets
 
 
-twitterclient = create_twitter_client()
+def create_tree(conversation_id, conversation_data):
+    """conversation_data should already be in the right chronological order"""
+    tree = Tree()
+    tree.create_node(conversation_id, conversation_id)  # Create Root Node
 
-dem_tweets, rep_tweets = get_legislator_tweets(twitterclient)
+    error_count = 0
+    for tree_datapoint in conversation_data:
+        try:
+            tree.create_node(
+                tree_datapoint[0], tree_datapoint[0], parent=tree_datapoint[1]
+            )
+        except:
+            error_count += 1
+    tree.show()
+    print(
+        f"Created Tree with {len(conversation_data) - error_count} replies. {error_count} replies were throwing "
+        f"an error due to deletions."
+    )
 
-conversation_id = dem_tweets[0][2]
-create_tree(conversation_id, twitterclient)
+
+if __name__ == "__main__":
+    twitterclient = create_twitter_client()
+
+    dem_tweets, rep_tweets = get_legislator_tweets(twitterclient)
+
+    now = datetime.datetime.now().strftime("%Y_%m_%d_%Y_%I_%M%p")
+
+    data_folder = Path("data/").mkdir(exist_ok=True)
+
+    # Save this already collected data in json files
+    json.dump(dem_tweets, open(f"data/dem_tweets_{now}.json ", "w"))
+    json.dump(rep_tweets, open(f"data/rep_tweets_{now}.json ", "w"))
+
+    # Get the convo data from twitter and write it in a file
+    for tweet in dem_tweets:
+        tweet["conversation_data"] = get_conversation_data(
+            tweet["conversation_id"], twitterclient
+        )
+    for tweet in rep_tweets:
+        tweet["conversation_data"] = get_conversation_data(
+            tweet["conversation_id"], twitterclient
+        )
+
+    json.dump(dem_tweets, open(f"data/dem_tweets_with_convo_{now}.json ", "w"))
+    json.dump(rep_tweets, open(f"data/rep_tweets_with_convo_{now}.json ", "w"))
+
+    # create_tree(conversation_id, conversation_data)
