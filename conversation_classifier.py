29f2e319fff4d4fe5b6618a22275f5e4619baa68
Constantin Eulenstein <constantineulenstein@DN0a23233b.SUNet>
2022-10-29 14:26:11 -0700

    Added dem and rep accuracy

--- conversation_classifier.py
+++ conversation_classifier.py
@@ -16,11 +16,11 @@ from sklearn.naive_bayes import GaussianNB
 def create_dataset(data, cutoff_size):
     X = []
     y = []
-    feature_names = [feature for feature in data[0].keys()][2:]
+    feature_names = [feature for feature in data[0].keys()][2:-1]
     for datapoint in data:
         if datapoint["size"] >= cutoff_size:
             X.append([datapoint[feature] for feature in feature_names])
-            y.append(datapoint["author_id"])
+            y.append(datapoint["party"])
         # X.append([datapoint["depth"], datapoint["size"], datapoint["width"], datapoint["structural_virality"],
         #          datapoint["density"], datapoint["diameter"], datapoint["reply_count"], datapoint["unique_users"]])
         # y.append(datapoint["author_id"])
@@ -32,12 +32,16 @@ def create_dataset(data, cutoff_size):
 def stratified_kFold(X, y, clf, verbose):
     skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
     lst_accu_stratified = []
+    lst_accu_dems_stratified = []
+    lst_accu_reps_stratified = []
 
     for train_index, test_index in skf.split(X, y):
         X_train_fold, X_test_fold = X[train_index], X[test_index]
         y_train_fold, y_test_fold = y[train_index], y[test_index]
         clf.fit(X_train_fold, y_train_fold)
         lst_accu_stratified.append(clf.score(X_test_fold, y_test_fold))
+        lst_accu_dems_stratified.append(clf.score(X_test_fold[np.argwhere(y_test_fold=="Democrat").flatten()], y_test_fold[y_test_fold=="Democrat"]))
+        lst_accu_reps_stratified.append(clf.score(X_test_fold[np.argwhere(y_test_fold=="Republican").flatten()], y_test_fold[y_test_fold=="Republican"]))
 
     if verbose:
         print('List of possible accuracy:', lst_accu_stratified)
@@ -47,6 +51,10 @@ def stratified_kFold(X, y, clf, verbose):
               min(lst_accu_stratified) * 100, '%')
         print('\nOverall Accuracy:',
               np.mean(lst_accu_stratified) * 100, '%')
+        print('\nOverall Accuracy on Democrats:',
+              np.mean(lst_accu_dems_stratified) * 100, '%')
+        print('\nOverall Accuracy on Republicans:',
+              np.mean(lst_accu_reps_stratified) * 100, '%')
         print('\nStandard Deviation is:', np.std(lst_accu_stratified))
 
     return np.mean(lst_accu_stratified)
@@ -59,13 +67,12 @@ def generate_ccdf_plots(dems, reps, feature):
         return x, 1 - cusum / cusum[-1]
 
     x_dems, y_dems = _ccdf(dems)
-    # x_dems = np.insert(x_dems, 0, 0.) #Add so plot always starts at 0
-    # y_dems = np.insert(y_dems, 0, 1.)
+    x_dems = np.insert(x_dems, 0, 0.) #Add so plot always starts at 0
+    y_dems = np.insert(y_dems, 0, 1.)
 
     x_reps, y_reps = _ccdf(reps)
-    # x_reps = np.insert(x_reps, 0, 0.)
-    # y_reps = np.insert(y_reps, 0, 1.)
-
+    x_reps = np.insert(x_reps, 0, 0.)
+    y_reps = np.insert(y_reps, 0, 1.)
     plt.xscale("log")
     plt.yscale("log")
     plt.plot(x_dems, y_dems, drawstyle='steps-post', label="Dems")
@@ -75,7 +82,7 @@ def generate_ccdf_plots(dems, reps, feature):
     plt.ylabel("CCDF (%)")
     current_values = plt.gca().get_yticks()
     plt.gca().set_yticklabels(['{:,.2%}'.format(x) for x in current_values])
-    # plt.tight_layout()
+    plt.tight_layout()
     plt.show()
 
 
@@ -96,9 +103,17 @@ if __name__ == "__main__":
     plot_distributions = False
     cutoff_size = 5
     data = json.load(open("conversation_metrics_v2.json"))
+
     feature_name_dict = {
-        name: idx for idx, name in enumerate(list(data[0].keys())[2:])
+        name: idx for idx, name in enumerate(list(data[0].keys())[2:-1])
     }
+
+    max_conv_dict = {}
+    for feature in feature_name_dict:
+        maxConv = max(data, key=lambda x: x[feature])
+        max_conv_dict[feature] = maxConv
+    json.dump(max_conv_dict, open("maxConvs.json", "w"))
+
     X, y = create_dataset(data, cutoff_size)
     print(f"The dataset contains {len(y[y == 'Democrat'])} conversations of Democrats and {len(y[y == 'Republican'])} "
           f"conversations of Republicans.")
@@ -109,7 +124,6 @@ if __name__ == "__main__":
             plot_distribution(X, y, feature_name_dict, feature)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
-
     names = [
         "Nearest Neighbors",
         "Linear SVM",
@@ -151,7 +165,7 @@ if __name__ == "__main__":
     clf.fit(X_train, y_train)
     print(f"\n The accuracy on final test set is {clf.score(X_test, y_test) * 100} %")
 
-    r = permutation_importance(clf, X_test, y_test, n_repeats=30, random_state=0)
+    r = permutation_importance(clf, X_test, y_test, n_repeats=100, random_state=0)
     for i in r.importances_mean.argsort()[::-1]:
         print(f"{[feature for feature in feature_name_dict if feature_name_dict[feature] == i][0]}: "
               f"{r.importances_mean[i]:.3f}"
